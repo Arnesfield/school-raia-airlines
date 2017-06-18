086ba3006bbba301b3ff02a5fd71b65871e1800b
Arnesfield <rylee.jeff385@gmail.com>
2017-06-18 19:33:29 +0800

    Started admin side

--- markup/admin-home.php
+++ markup/admin-home.php
@@ -1,3 +1,8 @@
-<h1>Hi admin</h1>
+<?php
+require_once('action/session-expire.php');
+require_once('markup/admin-nav.html');
 
-<a href="action/logout.php">&laquo; Logout</a>
+// require_once('action/db-connection.php');
+?>
+
+<h1>Hi admin</h1>
